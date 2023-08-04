cf8c773ef4bccd1b64a6b0328ea5ff7b698609f6
aaryac33 <102223660+aaryac33@users.noreply.github.com>
2023-08-04 19:49:54 +0530

    Add files via upload

---
+++ linux.py
@@ -0,0 +1,32 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+fn login_handler(info: web::Json<LoginInfo>) -> impl Responder {
+    // Your login logic here
+    if info.username == "example_user" && info.password == "example_pass" {
+        HttpResponse::Ok().json(LoginResponse { status: "Login successful".to_string() })
+    } else {
+        HttpResponse::Unauthorized().json(LoginResponse { status: "Login failed".to_string() })
+    }
+}
+
+#[derive(serde::Deserialize)]
+struct LoginInfo {
+    username: String,
+    password: String,
+}
+
+#[derive(serde::Serialize)]
+struct LoginResponse {
+    status: String,
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/login", web::post().to(login_handler))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
